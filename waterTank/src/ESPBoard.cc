a5364bac47a19be0249d0d110c275ee7fe7dc3cf
renegourley <rene@proto87.org>
2024-07-11 07:17:25 -0700

    Tidy up mp3 after experiments

--- waterTank/src/ESPBoard.cc
+++ waterTank/src/ESPBoard.cc
@@ -1,16 +1,32 @@
+#include <HardwareSerial.h>
 #include "Board.h"
 #include "ESPBoard.h"
 #include "Arduino.h"
 
-ESPBoard::ESPBoard(int motorPorts[]) {
+ESPBoard::ESPBoard(int motorPorts[], int mp3Tx, int mp3Rx) {
   _motorPorts = motorPorts;
   for (int i = 0; i<MOTOR_PIN_COUNT; i++) {
      pinMode(_motorPorts[i],OUTPUT);
   }
+
+  Serial2.begin(9600,SERIAL_8N1,mp3Rx,mp3Tx);
+  if (!_mp3.begin(Serial2)) throw "Failed to initialize mp3";
 }
 
 void ESPBoard::writeMotor(int pinValues[]) {
   for (int i=0; i<MOTOR_PIN_COUNT; i++) {
     digitalWrite(_motorPorts[i],pinValues[i]);
   }
+}
+
+void ESPBoard::mp3Play(uint track) {
+  _mp3.loop(track);
+}
+
+void ESPBoard::mp3Stop() {
+  _mp3.stop();
+}
+
+void ESPBoard::mp3Volume(uint volume) {
+  _mp3.volume(volume);
 }
--- waterTank/src/ESPBoard.h
+++ waterTank/src/ESPBoard.h
@@ -1,6 +1,7 @@
 #ifndef ESPBOARD_H
 #define ESPBOARD_H
 
+#include <DFRobotDFPlayerMini.h>
 #include "Board.h"
 #include "Constants.h"
 
@@ -9,9 +10,13 @@ class ESPBoard: public Board {
 private:
     int* _motorPorts;
     int _motorPinValues[MOTOR_PIN_COUNT];
+    DFRobotDFPlayerMini _mp3;
 public:
-    ESPBoard(int[4]); //MotorPorts
-    void writeMotor(int[4]);
+    ESPBoard(int motorPorts[4], int mp3Tx, int mp3Rx);
+    void writeMotor(int motorValues[4]);
+    void mp3Play(uint track);
+    void mp3Stop();
+    void mp3Volume(uint level);
 };
 
 #endif
